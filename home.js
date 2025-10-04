d41a57b74913596bac143ae98369a52a92b774b3
2625MRIU <ihsan0320malik@gmail.com>
2025-10-04 02:38:25 -0700

    Changes required

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ const fl = document.querySelector("#article1")
         fl.addEventListener('click',()=>{
             console.log("Click")
             const y = document.querySelector("#clicked")
-            y.setAttribute("href","../Article/article.html#20251001")
+            y.setAttribute("href","./Article/article.html#20251001")
             y.click()
         })
 
@@ -10,7 +10,7 @@ const f2 = document.querySelector("#article2")
         f2.addEventListener('click',()=>{
             console.log("Click")
             const y = document.querySelector("#clicked")
-            y.setAttribute("href","../Article/article.html#20250930")
+            y.setAttribute("href","./Article/article.html#20250930")
             y.click()
         })
 
@@ -18,6 +18,6 @@ const f3 = document.querySelector("#article3")
         f3.addEventListener('click',()=>{
             console.log("Click")
             const y = document.querySelector("#clicked")
-            y.setAttribute("href","../Article/article.html#20250929")
+            y.setAttribute("href","./Article/article.html#20250929")
             y.click()
         })
